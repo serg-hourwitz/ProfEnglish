4b751776f2e293b12d21433a9a7034bed2b480f6
Serhiy Hourwitz <shmone800@gmail.com>
2024-09-29 15:58:41 +0300

    added styles & js for arrow

---
+++ index.js
@@ -0,0 +1,20 @@
+const arrowUp = document.getElementById('arrow');
+
+// Обробник події прокрутки
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 400) {
+    arrowUp.style.opacity = '0.7';
+  } else {
+    arrowUp.style.opacity = '0';
+  }
+});
+
+// Обробник події при наведенні миші (hover)
+arrowUp.addEventListener('mouseover', () => {
+  arrowUp.style.opacity = '1'; // Збільшуємо прозорість при наведенні
+});
+
+// Обробник події, коли миша покидає елемент
+arrowUp.addEventListener('mouseout', () => {
+  arrowUp.style.opacity = '0.7'; // Повертаємо початкове значення прозорості
+});
