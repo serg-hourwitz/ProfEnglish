b7fe2ebc116265527b46b5a8d84f3029b7ebf7a1
Serhiy Hourwitz <shmone800@gmail.com>
2024-09-30 20:36:56 +0300

    added accessibility to video play buttons

--- js/video.js
+++ js/video.js
@@ -1,26 +1,39 @@
 import { videos } from './video-list.js';
 
+function loadVideo(index, placeholder) {
+  // Ховаємо постер
+  placeholder.style.display = 'none';
+
+  // Створюємо iframe для поточного відео
+  const iframe = document.createElement('iframe');
+  iframe.src = videos[index].src;
+  iframe.loading = 'lazy';
+  iframe.title = videos[index].title;
+  iframe.allow = 'encrypted-media; fullscreen;';
+  iframe.style.position = 'absolute';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.top = '0';
+  iframe.style.left = '0';
+  iframe.style.border = 'none';
+
+  // Додаємо iframe в батьківський div
+  placeholder.parentNode.appendChild(iframe);
+}
+
+// Додаємо обробники для кожного відео
 document
   .querySelectorAll('.video-placeholder')
   .forEach((placeholder, index) => {
+    // Обробник події для кліку
     placeholder.addEventListener('click', function () {
-      // Ховаємо поточний постер
-      this.style.display = 'none';
+      loadVideo(index, this);
+    });
 
-      // Створюємо iframe для поточного відео
-      const iframe = document.createElement('iframe');
-      iframe.src = videos[index].src;
-      iframe.loading = 'lazy';
-      iframe.title = videos[index].title;
-      iframe.allow = 'encrypted-media; fullscreen;';
-      iframe.style.position = 'absolute';
-      iframe.style.width = '100%';
-      iframe.style.height = '100%';
-      iframe.style.top = '0';
-      iframe.style.left = '0';
-      iframe.style.border = 'none';
-
-      // Вставляємо iframe в батьківський div
-      this.parentNode.appendChild(iframe);
+    // Обробник події для натискання клавіші Enter
+    placeholder.addEventListener('keydown', function (event) {
+      if (event.key === 'Enter') {
+        loadVideo(index, this);
+      }
     });
   });
